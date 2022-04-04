06b43d8c4ce7eda6b7c61fbdca11c3d2e968c406
kimdonghun <kdh462601@gmail.com>
2022-04-04 17:00:30 +0900

    2022_04_04

---
+++ 1bean/src/main/java/day1/review/BuyController.java
@@ -0,0 +1,26 @@
+package day1.review;
+
+public class BuyController {
+
+//	private int cnt;			//기본데이터 타입(참고용)
+	private BuyService service;		//private 접근권한의 전역변수 : 클래스의 프로퍼티
+									//프로퍼티가 클래스일때 의존관계가 생깁니다.
+									//BuyService 클래스에 사용하는 것을 "의존관계가 있다" 고 표현합니다.
+	
+	//프로퍼티에 값을 대입 또는 참조할때 1)생성자 2)setter 2가지 방법 *중요
+	//->클래스 타입일때는 이것을 "의존관계 주입"(Dependency Injection(DI))이라고 합니다.
+	public BuyController(BuyService service) {
+		System.out.println("BuyController 생성자**********");
+			this.service=service;
+	}
+	
+	public void setService(BuyService service) {
+		this.service = service;
+	}
+	//----------------------------------------------------- *중요
+	
+	public void buy() {
+		System.out.println("cont.roller buy() ~~~~ ");
+		service.buy();
+	}
+}
---
+++ 1bean/src/main/java/day1/review/ProduceDao.java
@@ -0,0 +1,17 @@
+package day1.review;
+
+public class ProduceDao {
+
+	private int cnt;
+	
+	public ProduceDao() {
+		System.out.println("ProduceDao 기본 생성자******");
+	}
+	public void setCnt(int cnt) {	
+		this.cnt = cnt;
+	}
+	
+	public void produce() {
+		System.out.println("상품을" + cnt + " 수량으로 생산합니다.");
+	}
+}
